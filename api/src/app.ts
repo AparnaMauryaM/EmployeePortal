6448458f72f122d7ccd0de853e67b9661f0d01fd
aparna_maurya <aparnam@mindfiresolutions.com>
2025-12-17 11:57:24 +0530

    Added cors module in API.

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,7 +1,9 @@
 import express from "express";
+import cors from "cors";
 import apiRoutes from "./routes/apiRoutes";
 
 const app = express();
+app.use(cors());
 
 app.use(express.json());
 app.use("/api", apiRoutes);
